81b47865bb7b6109fb3a309b623a14bccd164302
ElijahMoore200 <ELijahm390@nycstudents.net>
2025-12-15 10:45:39 -0500

    kljadlkf

--- buisinessSolution_textEditor.py
+++ buisinessSolution_textEditor.py
@@ -5,5 +5,26 @@ import tkinter as tk
 
 root = Tk()
 root.title("Meeting Name")
+
 textarea = tk.Text(root, width = 50, height = 10, wrap="word")
 T = Text(root, height = 5, width = 52) 
+
+textarea = tk.Text(root, width = 50, height = 10, wrap="word")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
+tk.mainloop()
